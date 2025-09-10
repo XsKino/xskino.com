aac2df759cbae60d5e26da755feeb2ae3d108dd0
Andrés Piñón <94037175+XsKino@users.noreply.github.com>
2025-09-10 01:21:31 -0600

    config satatic deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,10 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
 
-import vercel from '@astrojs/vercel/serverless'
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'server',
-  adapter: vercel(),
+  output: 'static',
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es'],
